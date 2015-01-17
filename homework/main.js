33d2d3f83cfd11146dc7c997ef06d05fd8a82a4e
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-01-17 17:59:45 -0600

    updates

--- homework/main.js
+++ homework/main.js
@@ -34,23 +34,58 @@
 // console.log(rot13decrypt("uryy|"));
 
 // 4.Create a function that takes an unencrypted string and an integer (n) and returns the ROT-N version of that string.
-function rot13n(entry, n)	{
-    var result = "";
-    n = (Math.floor(Math.random * 10);
-    console.log(n)
+// function rot13n(entry, n)	{
+//     var result = "";
+//     n = (Math.floor(Math.random * 10);
+//     console.log(n)
  
-    for(var i = 0; i < entry.length; i++)	{
+//     for(var i = 0; i < entry.length; i++)	{
         
-        var cc = entry[i].charCodeAt(0);
+//         var cc = entry[i].charCodeAt(0);
        
-        result += String.fromCharCode(cc+13);
-    }
-    return result;
-}
-console.log(rot13n('hello', 4));
+//         result += String.fromCharCode(cc+13);
+//     }
+//     return result;
+// }
+// console.log(rot13n('hello', 4));
 // 5.Create a javascript function that takes an array of student names and returns an array of randomly selected pairs of students (array of arrays).
+// function randomizeArray(arr) {
+//   var result = [];
+
+//   while (arr.length > 0) {
+//     var i = Math.floor(Math.random() * arr.length);
+//     result.push(arr[i]);
+//     arr.splice(i, 1);
+//   }
+//   console.log(result[1], result[2]);
+// }
+
+
+
+
+// var arr = ['Jacob', 'Erika', 'Charles', 'Paul', 'Ryan', 'Daniel', 'Alex', 'Travis', 'Gabe', "Hughie"]
+// randomizeArray(arr);
+
+
+
+
+
 // 6.Create a function that takes a string and returns a string with a dash (-) in between each character using .split and .join.
+
+
+
+ 	var myArray = ['Jacob', 'Erika', 'Charles', 'Paul', 'Ryan', 'Daniel', 'Alex', 'Travis', 'Gabe', "Hughie"];
+    Dash = myArray.join("-");
+
+console.log(Dash);
+
+
+
 // 7.Create a function that takes a string and returns a string with a dash (-) in between each character without using .split or .join.
+	var myArray = ['Jacob', 'Erika', 'Charles', 'Paul', 'Ryan', 'Daniel', 'Alex', 'Travis', 'Gabe', "Hughie"];
+	dashed = myArray.splice();
+
+console.log(newClass);	
 // 8.Create a function that takes two string strings and returns a string with a dash (-) in between each character without using .split or .join.
 // 9.Use $('selector').val() and $('selector').click() to re-create your number guessing game without using window.prompt. Use Bootstrap to build the page.
 // 10.Use $('selector').val() and $('selector').click() to re-create your slot machine without using window.prompt. Use Bootstrap to build the page.
